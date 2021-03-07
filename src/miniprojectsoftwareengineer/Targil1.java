37d01988ba044cb2d0b4037d93fe5e31804addde
itamar124812 <itamar124812@gmail.com>
2021-03-08 00:24:17 +0200

    fuck your wife

---
+++ src/miniprojectsoftwareengineer/Targil1.java
@@ -0,0 +1,12 @@
+package miniprojectsoftwareengineer;
+import javax.print.attribute.standard.MediaPrintableArea;
+
+
+public class Targil1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello world");
+	}
+
+}
