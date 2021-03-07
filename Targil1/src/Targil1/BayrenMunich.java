f711f875db956416686140b25f1b9018564cac28
Raz <razaviad@g.jct.ac.il>
2021-03-08 01:40:53 +0200

    Mr bachor

--- Targil1/src/Targil1/BayrenMunich.java
+++ Targil1/src/Targil1/BayrenMunich.java
@@ -5,6 +5,7 @@ public class BayrenMunich {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.print("Fuck your wife");
+System.out.print("Fuck your mom");
 	}
 
 }
