146a1c2613a657c7b26dee326b676777727ea4e6
itamar124812 <itamar124812@gmail.com>
2021-03-08 02:39:31 +0200

    Vector

---
+++ Targil1/src/primitives/Vector.java
@@ -0,0 +1,19 @@
+package primitives;
+
+public class Vector {
+	private Point3D head;
+	
+    public Vector(Coordinate a,Coordinate b,Coordinate c)
+    {
+	//head=new Point3D()
+    }
+	public Vector(Point3D head) {
+		super();
+		this.head = head;
+	}
+
+	public Point3D getHead() {
+		return head;
+	}
+
+}
