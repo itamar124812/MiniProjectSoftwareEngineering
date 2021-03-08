ddc227fc8f0b223d3f02ac07e26eb419d00f906c
Raz <razaviad@g.jct.ac.il>
2021-03-08 02:49:33 +0200

    push3

--- Targil1/src/primitives/Point3D.java
+++ Targil1/src/primitives/Point3D.java
@@ -5,11 +5,19 @@ public final class Point3D {
 	private final Coordinate y;
 	private final Coordinate z;
 	
+	final static Point3D ZERO =new Point3D(0,0,0);
+
 	public Point3D(Coordinate x, Coordinate y, Coordinate z) {
 		super();
 		this.x = x;
 		this.y = y;
 		this.z = z;
+	}
+	public Point3D(double x, double y, double z) {
+		super();
+		this.x = new Coordinate(x);
+		this.y = new Coordinate(y);
+		this.z = new Coordinate(z);
 	}
 	public Coordinate getX() {
 		return x;
@@ -28,5 +36,20 @@ public final class Point3D {
 	 Point3D other = (Point3D)obj;
 	 return this.x.equals(other.x) && this.y.equals(other.y)&& this.z.equals(other.z);
 	 }
-
+	@Override
+	public String toString() {
+		return "Point3D [ x= " + x.toString() + " , y= " + y.toString() + " , z= " + z.toString() + " ]";
+	}
+	public Vector subtract(Point3D point2) {
+	 
+	}
+	public Point3D add(Vector vector) {
+		 
+	}
+	public double distanceSquared(Point3D point2) {
+		 return this.x
+	}
+	public double distance(Point3D point2) {
+		 
+	}
 }
