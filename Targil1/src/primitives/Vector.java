4614ece26afbe502e2f7ac24687acf4f5d9bfb6f
itamar124812 <itamar124812@gmail.com>
2021-03-08 03:01:54 +0200

    Vector add and substract

--- Targil1/src/primitives/Vector.java
+++ Targil1/src/primitives/Vector.java
@@ -2,10 +2,12 @@ package primitives;
 
 public class Vector {
 	private Point3D head;
-	
+	public Vector(double x,double y,double z) {
+		head=new Point3D(x,y,z);
+	}
     public Vector(Coordinate a,Coordinate b,Coordinate c)
     {
-	//head=new Point3D()
+	head=new Point3D(a,b,c);
     }
 	public Vector(Point3D head) {
 		super();
@@ -15,5 +17,14 @@ public class Vector {
 	public Point3D getHead() {
 		return head;
 	}
+	public Vector add(Vector other) {
+		return new Vector(head.x.coord+other.head.x.coord,head.y.coord+other.head.y.coord,head.z.coord+other.head.z.coord);
+	}
+	public Vector subtract(Vector other) {
+		return new Vector(head.x.coord-other.head.x.coord,head.y.coord-other.head.y.coord,head.z.coord-other.head.z.coord);
+	}
+	//public Vector(double scalar) {
+		
+	//}
 
 }
