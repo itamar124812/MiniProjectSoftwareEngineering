040aead4abe8b29e7aba574f8f133ba9457f9ef7
Raz <razaviad@g.jct.ac.il>
2021-03-10 00:15:12 +0200

    the main working

---
+++ Targil1/src/geometries/Sphere.java
@@ -0,0 +1,14 @@
+package geometries;
+
+import primitives.Point3D;
+import primitives.Vector;
+
+public class Sphere implements Geometry {
+
+	@Override
+	public Vector getNormal(Point3D point) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
