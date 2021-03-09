20e902d6391dbfc6f9c6d7eecfaeb5e474f4163b
itamar124812 <itamar124812@gmail.com>
2021-03-09 23:51:16 +0200

    Plane.getnormal

--- Targil1/src/geometries/Plane.java
+++ Targil1/src/geometries/Plane.java
@@ -7,12 +7,18 @@ private Point3D q0;
 public Plane(Point3D a,Point3D b,Point3D c)
 {
 	q0=a;
-	
+	Vector A =a.subtract(b);
+	Vector B=a.subtract(c);
 }
+
 @Override
 public Vector getNormal(Point3D point) {
 	// TODO Auto-generated method stub
 	return null;
+}
+
+public Vector getNormal() {
+	return normal;
 }
 
 }
