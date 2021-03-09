c65b4b7608c770e6829f45a876f015910d1cb608
Raz <razaviad@g.jct.ac.il>
2021-03-09 23:46:07 +0200

    push2

---
+++ Targil1/src/primitives/Ray.java
@@ -0,0 +1,34 @@
+package primitives;
+
+public class Ray {
+private Point3D p0;
+private Vector dir;
+
+public Ray(Point3D p0, Vector dir) {
+	super();
+	this.p0 = p0;
+	if(this.dir.length()==1)
+	this.dir = dir;
+	else
+		this.dir.normalize();
+}
+
+public Point3D getP0() {
+	return p0;
+}
+public Vector getDir() {
+	return dir;
+}
+@Override
+public String toString() {
+	return "Ray [p0=" + p0.toString() + ", dir=" + dir.toString() + "]";
+}
+@Override
+public boolean equals(Object obj) {
+if (this == obj) return true;
+if (obj == null) return false;
+if (!(obj instanceof Ray)) return false;
+Ray other = (Ray)obj;
+return this.p0.equals(other.p0) && this.dir.equals(other.dir);
+}
+}
