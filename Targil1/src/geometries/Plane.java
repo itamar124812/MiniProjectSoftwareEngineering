d6523715774551773c10c70fed66f99c43ef725a
itamar124812 <itamar124812@gmail.com>
2021-03-09 23:47:28 +0200

    ש

--- Targil1/src/geometries/Plane.java
+++ Targil1/src/geometries/Plane.java
@@ -3,5 +3,16 @@ import primitives.Vector;
 import primitives.Point3D;
 public class Plane  implements Geometry {
 private Vector normal;
-private Point3D
+private Point3D q0;
+public Plane(Point3D a,Point3D b,Point3D c)
+{
+	q0=a;
+	
+}
+@Override
+public Vector getNormal(Point3D point) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
 }
