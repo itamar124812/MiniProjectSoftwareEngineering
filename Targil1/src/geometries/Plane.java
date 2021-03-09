4b43836bc477654ff704c1916ca65e23bec3e2a2
itamar124812 <itamar124812@gmail.com>
2021-03-10 00:36:53 +0200

    Final Plane

--- Targil1/src/geometries/Plane.java
+++ Targil1/src/geometries/Plane.java
@@ -9,9 +9,21 @@ public Plane(Point3D a,Point3D b,Point3D c)
 	q0=a;
 	Vector A =a.subtract(b);
 	Vector B=a.subtract(c);
-	
+	if(A.normalize().equals(B.normalize())||A.normalize().equals(B.normalize().scale(-1)))	throw new  IllegalArgumentException("Three points must not be on the same straight line.");
+	else 
+	{
+		normal =A.crossProduct(B);
+	}
 }
-
+public Plane(Point3D Q0,Vector Normal) 
+{
+	q0=Q0;
+	normal=Normal;
+}
+@Override
+public String toString() {
+	return "Plane [normal=" + normal + ", q0=" + q0 + "]";
+}
 @Override
 public Vector getNormal(Point3D point) {
 	// TODO Auto-generated method stub
@@ -20,6 +32,10 @@ public Vector getNormal(Point3D point) {
 
 public Vector getNormal() {
 	return normal;
+}
+
+public Point3D getQ0() {
+	return q0;
 }
 
 }
