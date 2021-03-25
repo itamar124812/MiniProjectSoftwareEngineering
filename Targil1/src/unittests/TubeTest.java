4f2a8d6af5aae43934014fad17fbb4752cffe07b
itamar124812 <itamar124812@gmail.com>
2021-03-25 03:59:02 +0200

    4:00

--- Targil1/src/unittests/TubeTest.java
+++ Targil1/src/unittests/TubeTest.java
@@ -19,14 +19,14 @@ public class TubeTest {
 
 	/**
 	 * Test method for {@link geometries.Tube#getNormal(primitives.Point3D)}.
-	 */
+	 */ 
 	@Test
 	public void testGetNormal() {
 		Point3D p=new Point3D(0,0,0);
 		Vector v=new Vector(0,0,1);
 		Ray r=new Ray(p,v);
 		Tube t= new Tube(r,1);
-		assertEquals(, 0);
+		assertEquals(t, 0);
 	}
 
 }
