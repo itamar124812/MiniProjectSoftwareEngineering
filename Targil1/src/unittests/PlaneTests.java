8ec683b5b30c22ca46aaf53cb3331526d24e0313
itamar124812 <itamar124812@gmail.com>
2021-03-25 02:57:14 +0200

    Plane Test

--- Targil1/src/unittests/PlaneTests.java
+++ Targil1/src/unittests/PlaneTests.java
@@ -4,7 +4,7 @@
 package unittests;
 
 import static org.junit.Assert.*;
-
+import static primitives.Util.isZero;
 import org.junit.Test;
 import geometries.Plane;
 import primitives.Point3D;
@@ -24,8 +24,8 @@ public class PlaneTests {
 		Point3D p2=new Point3D(2,4,6);
 		Point3D p3=new Point3D(-1,-1,1);
 		Plane p=new Plane(p1,p2,p3);
-		assertEquals(p.getNormal().length()==1||p.getNormal().length()==-1,true);
-		assertEquals(p.getNormal(),new Vector(5,-4,1));
+		assertEquals(isZero(p.getNormal().length()-1),true);
+		assertEquals(p.getNormal(),new Vector(5,-4,1).normalize());
 	}
 
 }
