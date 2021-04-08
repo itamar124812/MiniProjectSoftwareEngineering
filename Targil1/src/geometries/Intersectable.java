b5feca4aa9e07367b34c00126e83d5032c44cb84
itamar124812 <itamar124812@gmail.com>
2021-04-08 19:55:30 +0300

    commit

--- Targil1/src/geometries/Intersectable.java
+++ Targil1/src/geometries/Intersectable.java
@@ -3,10 +3,15 @@
  */
 package geometries;
 
+import java.util.List;
+
+import primitives.Point3D;
+import primitives.Ray;
+
 /**
  * @author USER
  *
  */
 public interface Intersectable  {
-
+	List<Point3D> findIntsersections(Ray ray);
 }
