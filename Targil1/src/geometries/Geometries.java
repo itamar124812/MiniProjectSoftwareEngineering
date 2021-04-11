45b3c2b8aa4e829f6488a71f461e5fed54037380
itamar124812 <itamar124812@gmail.com>
2021-04-11 23:42:35 +0300

    Geometries+Test

---
+++ Targil1/src/geometries/Geometries.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package geometries;
+
+import java.util.List;
+
+/**
+ * @author USER
+ *
+ */
+public class Geometries {
+
+	private List<Intersectable> intersectable;
+
+	public Geometries(List<Intersectable> intersectable) {
+		this.intersectable = intersectable;
+	}
+	
+}
---
+++ Targil1/src/unittests/GeometriesTests.java
@@ -0,0 +1,19 @@
+package unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GeometriesTests {
+
+	@Test
+	public void testGeometries() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindIntsersections() {
+		fail("Not yet implemented");
+	}
+
+}
