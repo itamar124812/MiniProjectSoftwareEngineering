01e85d7634fec77c7d7b9ed99888b6ece80f1879
Raz <razaviad@g.jct.ac.il>
2021-04-14 02:47:35 +0300

    findIntersections

--- Targil1/src/geometries/Geometries.java
+++ Targil1/src/geometries/Geometries.java
@@ -26,7 +26,17 @@ public class Geometries implements Intersectable {
 	}
 	@Override
 	public List<Point3D> findIntersections(Ray ray) {
+    	List<Point3D> result=new ArrayList<Point3D>(),temp=new ArrayList<Point3D>();
+		if(this.intersectable.isEmpty())
 		return null;
+		for(int i=0;i<this.intersectable.size();i++) {
+			 temp=this.intersectable.get(i).findIntersections(ray);
+			 if(temp!=null)
+				 result.addAll(temp);
+		}
+		if(result.isEmpty())
+			return null;
+		return result;
 	}
 	
 	
