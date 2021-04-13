e30069125b66fc5535ef7f6120bc6a01285e407f
itamar124812 <itamar124812@gmail.com>
2021-04-13 21:43:47 +0300

    nothing

--- Targil1/src/geometries/Cylinder.java
+++ Targil1/src/geometries/Cylinder.java
@@ -13,55 +13,60 @@ import primitives.Vector;
  * @author Itamar and Raz
  *
  */
-public class Cylinder extends Tube implements Geometry{
+public class Cylinder extends Tube implements Geometry {
 	private double height;
-	
+
 	public Cylinder(Ray axisRay, double radius, double height) {
 		super(axisRay, radius);
 		this.height = height;
 	}
+
 	public double getHeight() {
 		return height;
 	}
+
 	@Override
 	public String toString() {
 		return "Cylinder [height=" + height + ", toString()=" + super.toString() + "]";
 	}
+
 	@Override
 	/**
 	 * Calculate the normal for points on the bases and axis of a final cylinder.
 	 * 
 	 * @param point
-	 * @return Calculate the normal for points on the bases and axis of a final cylinder.
+	 * @return Calculate the normal for points on the bases and axis of a final
+	 *         cylinder.
 	 */
 	public Vector getNormal(Point3D point) {
-		if(point.equals(getAxisRay().getP0())||point.equals(getAxisRay().getP0().add(getAxisRay().getDir().scale(height))))return this.getAxisRay().getDir();
-		Vector a=(point.subtract(this.getAxisRay().getP0()));
-		Vector b=point.subtract(this.getAxisRay().getP0()).subtract(this.getAxisRay().getDir().scale(height));
-		if(a.dotProduct(this.getAxisRay().getDir())==0 || b.dotProduct(this.getAxisRay().getDir())==0 )
-		{
+		if (point.equals(getAxisRay().getP0())
+				|| point.equals(getAxisRay().getP0().add(getAxisRay().getDir().scale(height))))
 			return this.getAxisRay().getDir();
-		}
-		else if(super.getNormal(point)!=null) return super.getNormal(point);
-		else throw new IllegalArgumentException("The point is not on the cylinder.");		
-		
-		}
-	public List<Point3D> findIntersections(Ray ray) 
-	{
-		if(super.findIntersections(ray)==null) return null;
-		else 
-		{
-			List<Point3D> result=super.findIntersections(ray);
-			for(int i=0; i<result.size();i++)
-			{
-				if(result.get(i).getZ()>(super.getAxisRay().getP0().getZ()+this.height))
-					result.remove(i);			
-				else if(result.get(i).getZ()<super.getAxisRay().getP0().getZ())
-					result.remove(i);	
-			}			
+		Vector a = (point.subtract(this.getAxisRay().getP0()));
+		Vector b = point.subtract(this.getAxisRay().getP0()).subtract(this.getAxisRay().getDir().scale(height));
+		if (a.dotProduct(this.getAxisRay().getDir()) == 0 || b.dotProduct(this.getAxisRay().getDir()) == 0) {
+			return this.getAxisRay().getDir();
+		} else if (super.getNormal(point) != null)
+			return super.getNormal(point);
+		else
+			throw new IllegalArgumentException("The point is not on the cylinder.");
+
+	}
+
+	public List<Point3D> findIntersections(Ray ray) {
+		if (super.findIntersections(ray) == null)
+			return null;
+		else {
+			List<Point3D> result = super.findIntersections(ray);
+			for (int i = 0; i < result.size(); i++) {
+				if (result.get(i).getZ() > (super.getAxisRay().getP0().getZ() + this.height))
+					result.remove(i);
+				else if (result.get(i).getZ() < super.getAxisRay().getP0().getZ())
+					result.remove(i);
+			}
 			return result;
 		}
-			
+
 	}
 
 }
