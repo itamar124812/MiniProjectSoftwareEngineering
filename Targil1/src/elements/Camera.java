cd7592d13265b179ba12bd354f202c47d2688afb
itamar124812 <itamar124812@gmail.com>
2021-04-27 19:00:17 +0300

    htgcyh

--- Targil1/src/elements/Camera.java
+++ Targil1/src/elements/Camera.java
@@ -5,6 +5,9 @@ private Point3D location;
 private Vector Vto;
 private Vector Vright;
 private Vector Vup;
+private double Distance;
+private double Width;
+private double Height;
 public Camera(Point3D location, Vector vto, Vector vup) {
     this.location = location;
     Vto = vto;
@@ -13,7 +16,21 @@ public Camera(Point3D location, Vector vto, Vector vup) {
    {
       Vright=vup.crossProduct(vto);
    }
-    else throw new IllegalArgumentException("Vto must be ");
+    else throw new IllegalArgumentException("Vto must be orthogonal");
 }
+public Point3D getLocation() {
+    return location;
+}
+public Vector getVto() {
+    return Vto;
+}
+public Vector getVright() {
+    return Vright;
+}
+
+public Vector getVup() {
+    return Vup;
+}
+
 
 }
