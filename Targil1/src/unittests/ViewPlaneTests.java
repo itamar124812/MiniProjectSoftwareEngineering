448f7aca828e220bec9e8af078b94133e1baa55b
Raz <razaviad@g.jct.ac.il>
2021-04-27 19:19:10 +0300

    fghjk

--- Targil1/src/unittests/ViewPlaneTests.java
+++ Targil1/src/unittests/ViewPlaneTests.java
@@ -1,10 +1,31 @@
 package unittests;
 
 import primitives.*;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import elements.Camera;
 import geometries.*;
 
+/**
+ * Testing View Plane intresection
+ * 
+ * @author Itamar and Raz
+ *
+ */
 public class ViewPlaneTests {
+	/**
+	 * Test method for
+	 * {@link }.
+	 */
+	@Test
+	public void testConstructRayThroughPixel() {
+		Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0))
+				.setDistance(10);
 
+
+	}
 	
 }
