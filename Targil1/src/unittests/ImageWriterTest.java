8615a199fe52c5c23b1bf484e72fa8bd03fe0fb3
Raz <razaviad@g.jct.ac.il>
2021-05-13 12:11:56 +0300

    mr bajor

--- Targil1/src/unittests/ImageWriterTest.java
+++ Targil1/src/unittests/ImageWriterTest.java
@@ -1,5 +1,22 @@
 package unittests;
 
+import org.junit.Test;
+import renderer.*;
+import primitives.Color;
+
 public class ImageWriterTest {
 
+	@Test
+	public void ImageWriter() {
+		ImageWriter imageWriter=new ImageWriter("Test",800,500);
+		imageWriter.writeToImage();
+		for(int i=0;i<imageWriter.getNx();i++)
+			for(int j=0;j<imageWriter.getNy();j++) {
+				if(i%50==0||j%50==0)
+			    imageWriter.writePixel(i, j,new Color(java.awt.Color.YELLOW));
+			else
+				imageWriter.writePixel(i, j,new Color(java.awt.Color.BLUE));
+			}
+		imageWriter.writeToImage();
+	}
 }
