089d7f006f8ddb3a3956a5064d3f63b8ba93a61d
Raz <razaviad@g.jct.ac.il>
2021-05-16 01:25:50 +0300

    dfghj

--- Targil1/src/geometries/Sphere.java
+++ Targil1/src/geometries/Sphere.java
@@ -3,6 +3,7 @@ package geometries;
 import java.util.ArrayList;
 import java.util.List;
 
+import geometries.Intersectable.GeoPoint;
 import primitives.Point3D;
 import primitives.Ray;
 import primitives.Vector;
@@ -82,4 +83,35 @@ public String toString() {
 			return null;
 		return res;
 	}
-}
+	/**
+	 * @param ray
+	 * calculate intersection pGeoPoint between sphere and ray
+	 * 
+	 * @return List<GeoPoint> include the specific points
+	 */
+	@Override
+	public List<GeoPoint> findGeoIntersections(Ray ray){
+		List<GeoPoint> res=new ArrayList<GeoPoint>();
+		if(isZero(ray.getP0().getX()-center.getX()) && isZero(ray.getP0().getY()-center.getY()) &&
+				isZero(ray.getP0().getZ()-center.getZ())) {
+			res.add(new GeoPoint(this,ray.getPoint(this.radius)));
+			return res;
+		}
+		Vector u=this.center.subtract(ray.getP0());
+		double tm=alignZero(ray.getDir().dotProduct(u));
+		double d=alignZero(Math.sqrt(alignZero(u.lengthSquared() - tm*tm)));
+		if(d>=this.radius)
+		return null;
+		double th=alignZero(Math.sqrt(this.radius*this.radius-d*d));
+		double t1=alignZero(th+tm),t2=alignZero(tm-th);
+
+		 if(t1>0)
+			res.add(new GeoPoint(this,ray.getPoint(t1)));
+		 if(t2>0)
+			res.add(new GeoPoint(this,ray.getPoint(t2)));
+		 if(isZero(res.size()))
+			return null;
+		return res;
+	}
+	}
+
