5f1320cf6039d5639c333c21565cddd4651a49b7
itamar124812 <itamar124812@gmail.com>
2021-05-16 01:54:49 +0300

    nidsce

--- Targil1/src/primitives/Ray.java
+++ Targil1/src/primitives/Ray.java
@@ -11,56 +11,70 @@ import geometries.Intersectable.GeoPoint;
  *
  */
 public class Ray {
-private Point3D p0;
-private Vector dir;
- 
-public Ray(Point3D p0, Vector dir) {
-	super();
-	this.p0 = p0;
-	if(dir.length()==1)
-	this.dir = dir;
-	else
-		this.dir=dir.normalize();
-}
+	private Point3D p0;
+	private Vector dir;
 
-public Point3D getP0() {
-	return p0;
-}
-public Vector getDir() {
-	return dir;
-}
-@Override
-public String toString() {
-	return "Ray [p0=" + p0.toString() + ", dir=" + dir.toString() + "]";
-}
-@Override
-public boolean equals(Object obj) {
-if (this == obj) return true;
-if (obj == null) return false;
-if (!(obj instanceof Ray)) return false;
-Ray other = (Ray)obj;
-return this.p0.equals(other.p0) && this.dir.equals(other.dir);
-}
-public Point3D getPoint(double t) {
-	return this.p0.add(this.dir.scale(t));
-}
-/**
- * findClosestPoint
- * 
- * @param List<Point3D> 
- * @return the closest point to p0 ray
- */
-public Point3D findClosestPoint(List<Point3D> list) {
-	if(list==null)
-		return null;
-	Point3D close=list.get(0);
-	for(int i=1;i<list.size();i++)
-		if(this.p0.distance(close)>this.p0.distance(list.get(i)))
-			close=list.get(i);
+	public Ray(Point3D p0, Vector dir) {
+		super();
+		this.p0 = p0;
+		if (dir.length() == 1)
+			this.dir = dir;
+		else
+			this.dir = dir.normalize();
+	}
+
+	public Point3D getP0() {
+		return p0;
+	}
+
+	public Vector getDir() {
+		return dir;
+	}
+
+	@Override
+	public String toString() {
+		return "Ray [p0=" + p0.toString() + ", dir=" + dir.toString() + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Ray))
+			return false;
+		Ray other = (Ray) obj;
+		return this.p0.equals(other.p0) && this.dir.equals(other.dir);
+	}
+
+	public Point3D getPoint(double t) {
+		return this.p0.add(this.dir.scale(t));
+	}
+
+	/**
+	 * findClosestPoint
+	 * 
+	 * @param List<Point3D>
+	 * @return the closest point to p0 ray
+	 */
+	public Point3D findClosestPoint(List<Point3D> list) {
+		if (list == null)
+			return null;
+		Point3D close = list.get(0);
+		for (int i = 1; i < list.size(); i++)
+			if (this.p0.distance(close) > this.p0.distance(list.get(i)))
+				close = list.get(i);
 		return close;
-}
-public GeoPoint findClosestGeoPoint(List<GeoPoint> list)
-{
-	return null;
-}
+	}
+
+	public GeoPoint findClosestGeoPoint(List<GeoPoint> list) {
+		if (list == null)
+			return null;
+		GeoPoint close = list.get(0);
+		for (int i = 1; i < list.size(); i++)
+			if (this.p0.distance(close.point) > this.p0.distance(list.get(i).point))
+				close = list.get(i);
+		return close;
+	}
 }
--- Targil1/src/unittests/RayTests.java
+++ Targil1/src/unittests/RayTests.java
@@ -7,14 +7,41 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.Test;
 
+import geometries.Cylinder;
 import geometries.Intersectable;
+import geometries.Plane;
 import geometries.Triangle;
 import geometries.Tube;
+import geometries.Intersectable.GeoPoint;
 import primitives.Point3D;
 import primitives.Ray;
 import primitives.Vector;
 
 public class RayTests {
+	@Test
+	public void testfindGeoClosetPoint()
+	{
+		Ray ray=new Ray(new Point3D(0,0,0),new Vector(1,0,0));
+		Plane A=new Plane(new Point3D(1,2,3), new Point3D(2, 0, 1), new Point3D(0, 0, 1));
+		 // ============ Equivalence Partitions Tests ==============
+		 // TC01: The center point in the list is closest
+		List<GeoPoint> list=new ArrayList<GeoPoint>(List.of(new GeoPoint(A,new Point3D(20,0,0)),
+				new GeoPoint (A,new Point3D(12,0,0)),new GeoPoint(A,new Point3D(0,6,0)),new GeoPoint(new Plane(new Point3D(1,2,3), new Point3D(2, 0, 1), new Point3D(0, 0, 1)),new Point3D(50,20,0)),new GeoPoint(A,new Point3D(30,0,2))));
+   	 	assertEquals("the center point is closest", new GeoPoint(A,new Point3D(0,6,0)), ray.findClosestGeoPoint(list));
+   	 	
+		 // =============== Boundary Values Tests ==================
+		 // TC11: The list not contain points
+   	 assertNull("the list not contain points",ray.findClosestGeoPoint(null));
+   	 
+   	 	// TC12: The first point in the list is closest
+   	    list=new ArrayList<GeoPoint>(List.of(new GeoPoint(A,new Point3D(0,6,0)),new GeoPoint(A,new Point3D(20,0,0)),
+			new GeoPoint(null,new Point3D(12,0,0)),new GeoPoint(null,new Point3D(50,20,0)),new GeoPoint(A,new Point3D(30,0,2))));
+	 	assertEquals("the first point is closest", new GeoPoint(A,new Point3D(0,6,0)), ray.findClosestGeoPoint(list));   	 
+   	 	// TC13: The last point in the list is closest
+		list=new ArrayList<Point3D>(List.of(new Point3D(20,0,0),
+				new Point3D(12,0,0),new Point3D(50,20,0),new Point3D(30,0,2),new Point3D(0,6,0)));
+   	 	assertEquals("the last point is closest", new Point3D(0,6,0), ray.findClosestPoint(list));	}
+	}
 	
 	@Test
 	public void testfindClosestPoint() {
