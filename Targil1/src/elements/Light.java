04f1381faad2220b619870dd4a05a9eadfcd1ff6
itamar124812 <itamar124812@gmail.com>
2021-05-18 19:52:33 +0300

    Light

---
+++ Targil1/src/elements/Light.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package elements;
+
+import primitives.Color;
+
+/**
+ * @author USER
+ *
+ */
+abstract class Light {
+Color intensity;
+/**
+ * 
+ * @return intensity
+ */
+public Color getIntensity() {
+    return intensity;
+}
+/**
+ * 
+ * @param intensity
+ */
+protected Light(Color intensity) {
+    this.intensity = intensity;
+}
+
+
+
+ 
+}
