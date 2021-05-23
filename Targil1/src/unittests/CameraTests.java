76a24b348b93e67bcdf9a204816d7761546b2b67
itamar124812 <itamar124812@gmail.com>
2021-05-23 23:56:30 +0300

    x s

--- Targil1/src/unittests/CameraTests.java
+++ Targil1/src/unittests/CameraTests.java
@@ -54,5 +54,14 @@ public class CameraTests {
 				camera.setViewPlaneSize(6, 6).constructRayThroughPixel(3, 3, 0, 1));
 
 	}
+	@Test
+	public void moveAndRotateCamara()
+	{
+		Camera camera = new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0));
+		assertEquals(camera.moveCamara(new Point3D(1, 1, 1)).getLocation(),new Point3D(1, 1, 1) );
+		assertTrue(camera.rotateCamera(new Vector(0,0,1), Math.toRadians(45)).getVright().equals(new Vector(1/Math.sqrt(2),1/Math.sqrt(2), 0)));
+		assertEquals(camera.rotateCamera(new Vector(1,1,1), Math.toRadians(45)).getVright(),new Vector(-0.8047378541244,-0.5058793634017,0.310617217526));
+
+	}
 
 }
