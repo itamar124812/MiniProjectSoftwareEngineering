ccb40baa934387f26224984453f2b54d442fbebe
Raz <razaviad@g.jct.ac.il>
2021-06-07 01:28:27 +0300

    the scary movie

--- Targil1/src/unittests/MiniP.java
+++ Targil1/src/unittests/MiniP.java
@@ -15,6 +15,7 @@ import scene.Scene;
 import org.junit.Test;
 
 import elements.Camera;
+import elements.PointLight;
 import elements.SpotLight;
 import geometries.Intersectable;
 import geometries.Sphere;
@@ -24,23 +25,58 @@ public class MiniP {
     @Test
     public void Bear()
     {
-    	Camera camera = new Camera(new Point3D(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
-				.setViewPlaneSize(150, 150).setDistance(1000);
-    	Material mat=new Material().setkD(0.5).setks(0.5).setnShininess(100).setKr(0.2);
+    	Camera camera = new Camera(new Point3D(-78, -88, -1000), new Vector(0, 0, 1), new Vector(0, 1, 0)) //
+				.setViewPlaneSize(150, 150).setDistance(1500);
+    	Material mat=new Material().setkD(0.4).setks(0.4).setnShininess(300).setKr(0.2);
 		scene.geometries.add(new ArrayList<Intersectable>(List.of( //
 				new Sphere(new Point3D(-78, -88, -50),16) //
 						.setEmmission(new Color(218,165,32)) //
 						.setMaterial(mat),
-						new Sphere(new Point3D(0, 0, -50),10) //
+						new Sphere(new Point3D(-78, -62, -50),10) //
 						.setEmmission(new Color(218,165,32)) //
 						.setMaterial(mat),
-				new Sphere(new Point3D(0, 0, -50),25) //
+				new Sphere(new Point3D(-78, -68, -65),5) //
+						.setEmmission(new Color(139,69,19)) //
+						.setMaterial(mat),
+						new Sphere(new Point3D(-84.87, -55.82, -46.17),4.5) //
 						.setEmmission(new Color(218,165,32)) //
-						.setMaterial(mat))));
+						.setMaterial(mat),
+						new Sphere(new Point3D(-72.2, -55.01, -45.81),4.5) //
+						.setEmmission(new Color(218,165,32)),
+						new Sphere(new Point3D(-84.87, -55.82, -50),2.5) //
+						.setEmmission(new Color(139,69,19)) //
+						.setMaterial(mat),
+						new Sphere(new Point3D(-72.2, -55.01, -50),2.5) //
+						.setEmmission(new Color(139,69,19)) //
+						.setMaterial(mat),/////////////////////////////////////////////////////////
+						new Sphere(new Point3D(-81, -62, -65),2) //
+						.setEmmission(new Color(0,0,0)) //
+						.setMaterial(mat),
+						new Sphere(new Point3D(-75, -62, -65),2) //
+						.setEmmission(new Color(0,0,0)) //
+						.setMaterial(mat),
+						new Sphere(new Point3D(-78, -68, -72),2) //
+						.setEmmission(new Color(0,0,0)) //
+						.setMaterial(mat),/////////////////////////////////////////////////
+						new Sphere(new Point3D(-67.57, -79.27, -58.43),6) //
+						.setEmmission(new Color(139,69,19)) //
+						.setMaterial(mat),
+						new Sphere(new Point3D(-89.08, -78.26, -56.2),6) //
+						.setEmmission(new Color(139,69,19)) //
+						.setMaterial(mat),
+						new Sphere(new Point3D(-87.46, -98.73, -57.17),6) //
+						.setEmmission(new Color(139,69,19)) //
+						.setMaterial(mat),
+						new Sphere(new Point3D(-71.77, -100.69, -57.49),6) //
+						.setEmmission(new Color(139,69,19)) //
+						.setMaterial(mat)
+						)));
 		
-		scene.lights.add( //
-				new SpotLight(new Color(1000, 600, 0), new Point3D(-100, -100, 500), new Vector(-1, -1, -2)) //
-						.setkL(0.0004).setkQ(0.0000006));
+		scene.lights.add(new PointLight(new Color(255,0,0), new Point3D(-78,-150,-50)));
+		scene.lights.add(new PointLight(new Color(255,0,0), new Point3D(-100, -88, -50)));
+		scene.lights.add(new PointLight(new Color(255,0,0), new Point3D(-78, -120, -50)));
+		scene.lights.add(new PointLight(new Color(255,0,0), new Point3D(-78, -88, -85)));
+
 
 		Render render = new Render() //
 				.setImageWriter(new ImageWriter("Bearpo", 500, 500)) //
@@ -49,6 +85,5 @@ public class MiniP {
 		render.renderImage();
 		render.writeToImage();
 	}
-
-    }
+    
 }
